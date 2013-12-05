a1fc34b60c9130edcc98e2ab578ff14c18b6a510
Eric Sayle <esayle@onekingslane.com>
2013-12-05 14:56:41 -0800

    Now set up to have applications, too.

--- src/com/okl/tokenmgr/views.clj
+++ src/com/okl/tokenmgr/views.clj
@@ -81,9 +81,15 @@
      (gen-header (str my-name ":" app))
      [:body
       [:h1 (generate-breadcrumbs app)]
-      [:div {:id "testdiv" :style "width:80%;height:300px;"} ""]
+      [:div {:id "appdiv" :style "width:80%;height:300px;"} ""]
+      [:input {:type "submit" :class "AppAddNewRow" :value "Add New Row"}]
+      [:input {:type "submit" :class "AppSubmitChanges" :value "Submit Changes"}]
+      [:input {:type "submit" :class "AppResetChanges" :value "Reset Changes"}]
+      [:script {:type "text/javascript"}
+       (str "new AppSlickGrid('" app "', '"url-encoded-app "');")]
+      [:div {:id "tokendiv" :style "width:80%;height:300px;"} ""]
       [:input {:type "submit" :class "TokenAddNewRow" :value "Add New Row"}]
       [:input {:type "submit" :class "TokenSubmitChanges" :value "Submit Changes"}]
       [:input {:type "submit" :class "TokenResetChanges" :value "Reset Changes"}]
       [:script {:type "text/javascript"}
-       (str "tokenGrid = new TokenSlickGrid('" app "', '"url-encoded-app "');")]])))
+       (str "new TokenSlickGrid('" app "', '"url-encoded-app "');")]])))
