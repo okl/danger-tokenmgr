9e4e236f2cabbe095b65e428bdc0339c704469a7
Eric Sayle <esayle@onekingslane.com>
2014-01-06 18:14:28 -0800

    Updating project.clj.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject com.okl.tokenmgr "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-;  :url "http://example.com/FIXME"
+(defproject com.okl.tokenmgr "0.2.0"
+  :description "Store tokens in zookeeper and filter them"
+  :url "https://github.com/okl/danger-tokenmgr"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
                  [zookeeper-clj "0.9.3"]
