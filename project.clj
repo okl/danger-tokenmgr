5a5dce8a4dfbda949c0c7ef458378974a6e14b45
Eric Sayle <esayle@onekingslane.com>
2014-03-03 16:52:41 -0800

    tokenmgr 1.0.1 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.okl.tokenmgr "1.0.0"
+(defproject com.okl.tokenmgr "1.0.1"
   :description "Store tokens in zookeeper and filter them"
   :url "https://github.com/okl/danger-tokenmgr"
   :dependencies [[org.clojure/clojure "1.5.1"]
