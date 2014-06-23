4d22db7da7a994825180c2b91d7125ab770d333e
Tristan Willy <twilly@onekingslane.com>
2014-06-23 14:32:24 -0700

    move to v1.1.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.okl.tokenmgr "1.1.3"
+(defproject com.okl.tokenmgr "1.1.4-SNAPSHOP"
   :description "Store tokens in zookeeper and filter them"
   :url "https://github.com/okl/danger-tokenmgr"
   :dependencies [[org.clojure/clojure "1.5.1"]
